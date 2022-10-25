5ed85dda479734cb3d541ed75b86880e24ed7b8e
marinajulia <marinajulia707@gmail.com>
2022-10-25 15:16:05 -0300

    chore: Initial commit

---
+++ TestesUnitarios/FileClass.cs
@@ -0,0 +1,13 @@
+﻿namespace TestesUnitarios
+{
+    public class FileClass
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)){
+                throw new ArgumentNullException("FileName");
+            }
+            return File.Exists(fileName);
+        }
+    }
+}
